5cf0ff51d1613224e68e6149bac9087c3b8a8124
Jon <jondimanzanos@gmail.com>
2020-11-18 14:20:52 +0100

    4Ejercicio

--- Ejercicio2Ebalu1/src/Ejercicio4.java
+++ Ejercicio2Ebalu1/src/Ejercicio4.java
@@ -1,58 +1,81 @@
 import java.sql.Date;
+import java.util.Scanner;
 public class Ejercicio4 {
 
 	public static void main(String[] args) {
 		// Programa que pida por separado el día, el mes y el año de una fecha correcta y
 		// muestre por pantalla la fecha del día siguiente.
 		// VARIABLES
-		int dia,mes,año;
+		Scanner sc=new Scanner(System.in);
+		int dia=1,mes=1,año=1, limitesDias = 1;
+		dia=limitesDias;
 		System.out.println("-------FECHA-------");
 		System.out.println("Introduzca el año: ");
+		año=sc.nextInt();
 		System.out.println("Introduzca el mes: ");
+		mes=sc.nextInt();
 		System.out.println("Introduzca el dia: ");
+		dia=sc.nextInt();
+		
+			switch (mes) {
+			case 1: //ENERO
+				limitesDias=31;
+				dia++;
+				break;
+			case 2: //FEBRERO
+				if (año%4==0 && año%400==0) {
+					limitesDias=29;
+					dia++;
+					if (dia==30) {
+						mes=3;
+						dia=1;
+					}
+				}else {
+					limitesDias=28;
+					dia++;
+					if (dia==29) {
+						mes=3;
+						dia=1;
+					}
+				}
+				break;
+			case 3: //MARZO
+				limitesDias=31;
+				break;
+			case 4: //ABRIL
+				limitesDias=30;
+				break;
+			case 5: //MAYO
+				limitesDias=31;
+				break;
+			case 6: //JUNIO
+				limitesDias=30;
+				break;
+			case 7: //JULIO
+				limitesDias=31;
+				break;
+			case 8: //AGOSTO
 
-		switch (mes) {
+				break;
+			case 9: //SEPTIEMBRE
 
-		case 1:
+				break;
+			case 10: //OCTUBRE
 
-			break;
-		case 2:
+				break;
+			case 11: //NOVIEMBRE
 
-			break;
-		case 3:
+				break;
+			case 12: //DICIEMBRE
+				limitesDias=31;
+				break;
 
-			break;
-		case 4:
+			default:
+				System.out.println("Fecha introducida no valdia!");
+				break;
 
-			break;
-		case 5:
-
-			break;
-		case 6:
-
-			break;
-		case 7:
-
-			break;
-		case 8:
-
-			break;
-		case 9:
-
-			break;
-		case 10:
-
-			break;
-		case 11:
-
-			break;
-		case 12:
-
-			break;
-
-		default:
-			break;
-		}
+			}
+			System.out.println(año+"-"+mes+"-"+dia);
 	}
 }
 
