ca04eb70d39276ce2a7a2ee52561e498ca804ac2
JonMa489 <jondimanzanos@gmail.com>
2020-11-18 20:49:35 +0100

    Actualizacion

--- Ejercicio2Ebalu1/src/Ejercicio5.java
+++ Ejercicio2Ebalu1/src/Ejercicio5.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
 
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
 		//Programa que pida un número “n” por teclado y muestre por pantalla la
 		//multiplicación de los “n” primeros números impares.
+		@SuppressWarnings({ "unused", "resource" })
+		Scanner sc=new Scanner(System.in);
+		//VARIABLES
+		int num,dig = 0, multi = 0;
+		System.out.println("------Multiplicacion de numeros impares------");
+		System.out.println("Introduzca el numero: ");
+		num=sc.nextInt();
+		//CALCULO
+		while (num>0) {
+			dig=num%10;
 
-
+			if (dig%2==0) {
+				multi=multi+dig;
+			}
+			num/=10;
+		}
+		System.out.println("La multiplicacion es: "+ multi);
 	}
-
 }
+
+
--- Ejercicio2Ebalu1/src/Ejercicio8.java
+++ Ejercicio2Ebalu1/src/Ejercicio8.java
@@ -7,20 +7,26 @@ public class Ejercicio8 {
 		Scanner sc=new Scanner(System.in);
 		int[] numeros = new int[10];
 		// 	Programa que mediante llamadas a funciones llene un array, lo muestre con su
-		//	contenido y después lo muestre por pantalla tal y como se indica en la otra
-		//	función:
-		//	● 	Función para llenar un array de 10 elementos con números enteros entre
+		//	contenido y despuÃ©s lo muestre por pantalla tal y como se indica en la otra
+		//	funciÃ³n:
+		//	â—� 	FunciÃ³n para llenar un array de 10 elementos con nÃºmeros enteros entre
 		//		1 y 50.
-		//	● 	Función para mostrar el array con su contenido.
-		//	● 	Función para mostrarlos en el siguiente orden: el primero, el último, el
-		//		segundo, el penúltimo, el tercero, etc.
+		//	â—� 	FunciÃ³n para mostrar el array con su contenido.
+		//	â—� 	FunciÃ³n para mostrarlos en el siguiente orden: el primero, el Ãºltimo, el
+		//		segundo, el penÃºltimo, el tercero, etc.
 		llenarArray(numeros);
+		orden(numeros);
 	}
 	public static void llenarArray  (int[]numeros) {
 		for (int i = 0; i < numeros.length; i++) {
 			numeros[i]=(int)(Math.random()*49+1);
 			System.out.print(numeros[i]+ " ");
 		}
-		
+	}
+	public static void mostrar () {
+
+	}
+	public static void orden (int numeros[]) {
+	int j=4;
 	}
 }
